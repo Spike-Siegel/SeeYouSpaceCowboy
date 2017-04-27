f222518b3199d78ad23e3329a11aef3020559833
xinhuixu <x.xu.1102@gmail.com>
2017-04-27 14:46:16 -0400

    Update fibtest.py

--- fibtest.py
+++ fibtest.py
@@ -1,5 +1,8 @@
 #LVXX
 def fib(n):
-    if n == 0: return 0
-    elif n == 1: return 1
-    else: return fib(n-1)+fib(n-2)
+    if n == 0: 
+        return 0
+    elif n == 1: 
+        return 1
+    else: 
+        return fib(n-1)+fib(n-2)
