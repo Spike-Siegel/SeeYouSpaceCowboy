21865bf525a0f45b72dd9ae4452e77a4eeaf9f35
Lorenz Vargas <lvargas@stuy.edu>
2017-04-28 14:51:27 -0400

    merged

--- timetest.py
+++ timetest.py
@@ -1,6 +1,14 @@
 #LVXX
 import time
-from fibtest import fib
+
+#LVXX
+
+def argtest( f ):
+    def inner( *args, **kwargs ):
+        print f.func_name + str(args)
+        return f(*args, **kwargs )
+    return inner
+
 
 def timetest( f ):
     origtime = time.time()
@@ -10,6 +18,19 @@ def timetest( f ):
     print "Function runtime: " + str(functime)
     return inner
 
+@argtest
+def fib(n):
+    
+    if n == 0: 
+        return 0
+    elif n == 1: 
+        return 1
+    else: 
+        return fib(n-1)+fib(n-2)
+
+print 'result: ', fib(3)
+
+'''
 @timetest
 def fib(n):
     if n == 0:
@@ -18,3 +39,4 @@ def fib(n):
         return 1
     else:
         return fib(n-1)+fib(n-2)
+'''
